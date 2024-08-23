34f76f2b564a016a66a56dd3d50c45f1260420ae
LSITC203 <chintukarthik.1510@gmail.com>
2024-08-23 09:11:04 +0530

    added only index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const user=require("./routes/users.js")
+const product=require("./routes/products.js")
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.use("/users",user);
+app.use("/products",product);
+
+app.get("/ids",(req,res)=>{
+    const id=req.query.id;
+    const name=req.query.name;
+    res.json(name)
+})
+
+app.listen(3000)
